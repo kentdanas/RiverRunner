d151e51aaa90dad2687330e2f97dad571700dafe
lukeWaninger <luke.waninger@gmail.com>
2018-05-29 14:13:53 -0700

    sort dropdown alphabetically

--- riverrunner/daily.py
+++ riverrunner/daily.py
@@ -95,8 +95,8 @@ def daily_run():
     context = Context(settings.DATABASE)
     session = context.Session()
 
-    #get_weather_observations(session)
-    #get_usgs_observations()
+    get_weather_observations(session)
+    get_usgs_observations()
     compute_predictions(session)
 
     session.close()
