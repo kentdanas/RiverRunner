1928703d096d3e0527cde984da85d281b9364a37
lukeWaninger <luke.waninger@gmail.com>
2018-06-04 12:31:24 -0700

    updated host

--- riverrunner/ui.py
+++ riverrunner/ui.py
@@ -409,4 +409,4 @@ def update_dropdown(marker=None):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='127.0.0.1', port=8050)
+    app.run_server(debug=False, host='127.0.0.1', port=8050)
