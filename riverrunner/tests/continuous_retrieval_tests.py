b963fd507e5112b3a53d1609d72a20ee570a05ae
lukeWaninger <luke.waninger@gmail.com>
2018-06-06 01:25:34 -0700

    added test in continuous retrieval

--- riverrunner/tests/continuous_retrieval_tests.py
+++ riverrunner/tests/continuous_retrieval_tests.py
@@ -122,3 +122,17 @@ class TestRepository(TestCase):
             Measurement.date_time >= dt.datetime.now() - dt.timedelta(days=24)
         ).all()
         self.assertTrue(len(b) > len(a))
+
+    def test_fill_noaa_gaps(self):
+        station = self.context.get_stations_for_test(1, self.session)[0]
+        station.source = 'NOAA'
+        station.latitude = 47,
+        station.longitude = 122
+        self.session.add(station)
+
+        start = dt.datetime.now()
+        start = dt.datetime(year=start.year, month=start.month, day=start.day) - dt.timedelta(days=1)
+        end_date = dt.datetime.now()
+
+        t = fill_noaa_gaps(start, end_date, settings.DATABASE_TEST)
+        self.assertEqual(t, 0)
